ae6ec462f799f4eb50115bf37d367d6783f868ba
Mipusatina <mipspam2@mail.ru>
2024-03-21 18:48:08 +0100

    Sem 4. DZ. 6

--- Sem4/src/main/java/Db.java
+++ Sem4/src/main/java/Db.java
@@ -6,27 +6,45 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.Service;
 import org.hibernate.service.ServiceRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Db {
     private static final String URL = "jdbc:mysql://db4free.net:3306";
     private static final String USER = "gb20240321root";
     private static final String PASSWORD = "gb20240321pass";
 
-    public static void con(){
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure()
-                .build();
-        SessionFactory sessionFactory = new MetadataSources(registry)
-                .buildMetadata().buildSessionFactory();
-        Session session = sessionFactory.openSession();
-
-        Course course1 = new Course("Java", "Ivanov", 1000);
-
-        session.beginTransaction();
-        session.save(course1);
-        session.getTransaction().commit();
+    public static void con() {
+//        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+//                .configure()
+//                .build();
+//        SessionFactory sessionFactory = new MetadataSources(registry)
+//                .buildMetadata().buildSessionFactory();
+//        Session session = sessionFactory.openSession();
+//
+//        Course course1 = new Course("Java", "Ivanov", 1000);
+//
+//        session.beginTransaction();
+//        session.save(course1);
+//        session.getTransaction().commit();
+//        session.close();
+        Connector connector = new Connector();
+        Session session = connector.getSesion();
+        insertCoureses(session);
         session.close();
+    }
 
+    public static void insertCoureses(Session session){
+        List<Course> courses = new ArrayList<>();
+        courses.add(new Course("Java", "Ivanov", 1000));
+        courses.add(new Course("MySQL", "Petrov", 100));
+        courses.add(new Course("Hibernate", "Sidorov", 100));
+        courses.add(new Course("Docker", "Pushkin", 100));
 
-
+        session.getTransaction();
+        for(Course course: courses){
+            session.save(course);
+        }
+        session.beginTransaction().commit();
     }
 }
